197403f91d2a9b557397b889db64729f73de0c05
namrata44 <103646973+namrata44@users.noreply.github.com>
2023-03-28 18:26:34 +0530

    String

---
+++ String/src/com/string/String1.java
@@ -0,0 +1,19 @@
+package com.string;
+
+public class String1 {
+	
+		  public static void main(String[] args) {
+		    
+		    
+		    String first = "Java";
+		    String second = "Python";
+		    String third = "JavaScript";
+
+		    
+		    System.out.println(first);   
+		    System.out.println(second);  
+		    System.out.println(third);   
+		  }
+		}
+
+
---
+++ String/src/com/string/String2.java
@@ -0,0 +1,17 @@
+package com.string;
+
+public class String2 {
+	
+		  public static void main(String[] args) {
+
+		    
+		    String greet = "Hello! World";
+		    System.out.println("String: " + greet);
+
+		    
+		    int length = greet.length();
+		    System.out.println("Length: " + length);
+		  }
+		}
+
+
